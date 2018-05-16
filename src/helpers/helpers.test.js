4d49231d55e5a456c9a2430f78c49263860c228d
Samya <khemri.samya.isart@gmail.com>
2018-05-15 21:01:54 -0400

    add tests

--- src/helpers/helpers.test.js
+++ src/helpers/helpers.test.js
@@ -1,9 +1,86 @@
-import { isValidMail } from './helpers'
+import { 
+	isValidMail,
+	isValidPhone,
+	isMember,
+	isBanned
+ } from './helpers'
+
+const members = [
+	{
+		'mail': 'samya@mail.com',
+		'firstName': 'Samya',
+		'lastName': 'Khemri',
+		'phone': '5147124991',
+		'hasBike': false
+	},
+	{
+		'mail': 'kdog@mail.com',
+		'firstName': 'Kevin',
+		'lastName': 'Massicotte',
+		'phone': '5141234567',
+		'hasBike': false
+	},
+	{
+		'mail': 'ban@mail.com',
+		'firstName': 'Dark',
+		'lastName': 'Vador',
+		'phone': '5147894561',
+		'hasBike': false,
+		'isBanned': true
+	}
+]
 
 test('mail to be valid', () => {
-    expect(isValidMail('samya@mail.com')).toBe(true)
+	expect(isValidMail('samya@mail.com')).toBe(true)
 })
 
 test('mail to be invalid', () => {
-    expect(isValidMail('hello-world')).toBe(false)
+	expect(isValidMail('hello-world')).toBe(false)
+})
+
+test('phone to be valid', () => {
+	expect(isValidPhone('5127124991')).toBe(true)
+})
+
+test('phone to be invalid', () => {
+	expect(isValidPhone('hello-world')).toBe(false)
+})
+
+test('to be a member', () => {
+	const expected = {
+		'mail': 'samya@mail.com',
+		'firstName': 'Samya',
+		'lastName': 'Khemri',
+		'phone': '5147124991',
+		'hasBike': false
+	}
+	expect(isMember('samya@mail.com', members)).toEqual(expected)
+})
+
+test('to not be a member', () => {
+	expect(isMember('test@mail.com', members)).toBe(undefined)
+})
+
+test('to not be banned', () => {
+	const expected = {
+		'mail': 'samya@mail.com',
+		'firstName': 'Samya',
+		'lastName': 'Khemri',
+		'phone': '5147124991',
+		'hasBike': false
+	}
+	expect(isMember('samya@mail.com', members)).toEqual(expected)
+})
+
+test('to be banned', () => {
+	const expected = {
+		'mail': 'ban@mail.com',
+		'firstName': 'Dark',
+		'lastName': 'Vador',
+		'phone': '5147894561',
+		'hasBike': false,
+		'isBanned': true
+	}
+
+	expect(isMember('ban@mail.com', members)).toEqual(expected)
 })
---
+++ tests/e2e/login.js
@@ -0,0 +1,28 @@
+module.exports = {
+	'Test title SkyBikes' : function (browser) {
+	  browser
+		.url('http://localhost:8080')
+		.waitForElementVisible('body', 1000)
+		.assert.title('localhost', 'The <title> is correct')
+		.end()
+	},
+	'Login user for sysadmin' : function (browser) {
+	  browser
+		.url('http://localhost:8080')
+		.waitForElementVisible('#login')
+	  // .waitForElementVisible('input[name=lmail]')
+	  // .setValue('input[name=lmail]', 'kdog@mail.com')
+	  // .click('input[name=submit]')
+	  // .waitForElementVisible('#sysAdmin')
+	  .end()
+	},
+	'Login user for citizen worker' : function (browser) {
+	  browser
+	  .url('http://localhost:8080')
+	  .waitForElementVisible('input[name=lmail]')
+	  .setValue('input[name=lmail]', 'samya@mail.com')
+	  .click('input[name=submit]')
+	  .waitForElementVisible('#cityWorker')
+	  .end()
+	}
+  }
